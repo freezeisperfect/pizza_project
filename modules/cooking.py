b85b0c414bce81e2c3cb5543a3cc9a564a38f061
Daniil Zotov <mrdanka3@gmail.com>
2023-11-12 18:39:17 +0300

    Updated cooking module

--- modules/cooking.py
+++ modules/cooking.py
@@ -1,7 +1,10 @@
+import inspect
+
+
 class Pizza:
     actual_pizza_sizes = ['L', 'XL']
 
-    def __init__(self, pizza_size: str = 'L') -> None:
+    def __init__(self, pizza_size: str = 'XL') -> None:
         if not isinstance(pizza_size, str):
             raise ValueError
         if pizza_size not in self.actual_pizza_sizes:
@@ -9,30 +12,57 @@ class Pizza:
 
         self.receipt = None
         self.size = pizza_size
+        self.name = str(self)
+
+    def __str__(self) -> str:
+        return self.__class__.__name__
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Pizza):
+            raise ValueError
+        return self.name == other.name \
+            and self.size == other.size \
+            and set(self.receipt) == set(other.receipt)
+
+    def cook(self) -> None:
+        print(f'LET HIM COOK! (◕‿◕) {str(self)} is cooking!')
 
     def dict(self) -> dict[str, list[str]]:
-        pizza_name = self.__class__.__name__
-        return {pizza_name: self.receipt}
+        return {str(self): self.receipt}
 
 
 class Margherita(Pizza):
-    def __init__(self, pizza_size):
+    def __init__(self, pizza_size: str = 'XL'):
         super().__init__(pizza_size)
         self.receipt = ['tomato sauce', 'mozzarella', 'tomatoes']
 
 
 class Pepperoni(Pizza):
-    def __init__(self, pizza_size):
+    def __init__(self, pizza_size: str = 'XL'):
         super().__init__(pizza_size)
         self.receipt = ['tomato sauce', 'mozzarella', 'pepperoni']
 
 
 class Hawaiian(Pizza):
-    def __init__(self, pizza_size):
+    def __init__(self, pizza_size: str = 'XL'):
         super().__init__(pizza_size)
         self.receipt = ['tomato sauce', 'mozzarella', 'chicken', 'pineapples']
 
 
+def get_pizzas():
+    current_module = inspect.getmodule(inspect.currentframe())
+    classes = {
+        name.lower(): cls for name, cls in inspect.getmembers(current_module)
+        if inspect.isclass(cls) and name != 'Pizza'
+    }
+    return classes
+
+
 if __name__ == '__main__':
-    my_pizza = Margherita('XL')
+    my_pizza = Margherita()
     print(my_pizza.dict())
+    my_pizza.cook()
+    print(my_pizza.name)
+
+    my_margherita = Margherita()
+    print(my_margherita == my_pizza)
