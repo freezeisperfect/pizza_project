a3f08bcf7f62567db04782e0eab9f5a97273a4c5
Daniil Zotov <mrdanka3@gmail.com>
2023-11-15 00:00:36 +0300

    Added docstrings in cli module

--- modules/cli.py
+++ modules/cli.py
@@ -8,6 +8,9 @@ ACTUAL_PIZZAS = get_pizzas()
 
 @click.group
 def cli():
+    """
+    CLI-интерфейс для заказа пиццы (order) и вывода меню (menu).
+    """
     pass
 
 
@@ -15,7 +18,31 @@ def cli():
 @click.option('--delivery', default=False, is_flag=True)
 @click.option('--size', default='XL')
 @click.argument('pizza', nargs=1)
-def order(pizza: str, delivery: bool, size: str):
+def order(pizza: str, delivery: bool, size: str) -> None:
+    """
+    Выполнение заказа пиццы с опцией выбора доставки (или самовывоза при
+    ее отсуствтвии), а также размера пиццы.
+
+    Parameters
+    ----------
+    pizza: str
+        Название пиццы для заказа, передается как аргумент при вызове order.
+    delivery: bool
+        Флаг доставки, если True - то заказ будет доставлен, иначе - заказ
+        для самовывоза. Задается флагом --delivery при вызове order.
+    size: str
+        Размер пиццы для заказа. Задается как опциональный аргумент с помощью
+        флага --size при вызове order.
+
+    Returns
+    -------
+    None
+
+    Usage
+    -----
+    $ python cli.py order --delivery --size L Margherita
+    $ python cli.py menu
+    """
     if not isinstance(pizza, str) or not isinstance(size, str):
         raise ValueError
     if pizza.lower() not in ACTUAL_PIZZAS.keys():
@@ -28,7 +55,18 @@ def order(pizza: str, delivery: bool, size: str):
 
 
 @cli.command()
-def menu():
+def menu() -> None:
+    """
+    Вывод меню пицц для заказа с ингридиентами для ее изготовления.
+
+    Returns
+    -------
+    None
+
+    Usage
+    -----
+    $ python cli.py menu
+    """
     for name, pizza in ACTUAL_PIZZAS.items():
         pizza_item = pizza()
         click.echo(
