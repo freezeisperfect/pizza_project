98e17480aaea03882f57ac8706e2dbf1ae891e9d
Daniil Zotov <mrdanka3@gmail.com>
2023-11-14 22:38:11 +0300

    Updated CLI module with menu functionality

--- modules/cli.py
+++ modules/cli.py
@@ -3,6 +3,9 @@ from cooking import get_pizzas
 from delivery import Delivery
 
 
+ACTUAL_PIZZAS = get_pizzas()
+
+
 @click.group
 def cli():
     pass
@@ -13,16 +16,27 @@ def cli():
 @click.option('--size', default='XL')
 @click.argument('pizza', nargs=1)
 def order(pizza: str, delivery: bool, size: str):
-    actual_pizzas = get_pizzas()
-    if not isinstance(pizza, str):
+
+    print(ACTUAL_PIZZAS)
+    if not isinstance(pizza, str) or not isinstance(size, str):
         raise ValueError
-    if pizza.lower() not in actual_pizzas.keys():
+    if pizza.lower() not in ACTUAL_PIZZAS.keys():
         raise TypeError
 
-    pizza_object = actual_pizzas[pizza.lower()](size)
+    pizza_object = ACTUAL_PIZZAS[pizza.lower()](size)
     pizza_object.cook()
 
     Delivery(delivery, pizza_object).get_pizza()
+
+
+@cli.command()
+def menu():
+    for name, pizza in ACTUAL_PIZZAS.items():
+        pizza_item = pizza()
+        click.echo(
+            f'- {pizza_item.name} {pizza_item.emoji}: ' +
+            ', '.join(pizza_item.receipt)
+        )
 
 
 if __name__ == '__main__':
