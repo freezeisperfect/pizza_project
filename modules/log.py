f1e8eae92f294299d78e94f469f69f7b05732460
Daniil Zotov <mrdanka3@gmail.com>
2023-11-15 23:00:05 +0300

    Updated log module with wraps to get access to __wrapped__ attribute for testing

--- modules/log.py
+++ modules/log.py
@@ -1,5 +1,6 @@
 from random import randint
 from typing import Callable, Any
+from functools import wraps
 
 
 def log(print_template: str) -> Callable:
@@ -17,6 +18,7 @@ def log(print_template: str) -> Callable:
         Декорированная функция.
     """
     def decorator(func: Callable) -> Callable:
+        @wraps(func)
         def log_wrapper(*args: Any, **kwargs: Any):
             result = func(*args, **kwargs)
             print(print_template.format(randint(1, 10)))
